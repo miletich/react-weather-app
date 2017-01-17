0066c39e35cc0cd02134faea9d2000d990bb22ce
Dušan Miletić <d@miletich.cc>
2017-01-17 14:04:18 +0100

    Elaborate navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 
 class Navigation extends Component {
   render() {
     return (
       <div>
         <h1>Navigation</h1>
-        <Link to="/">Get Weather</Link>
-        <Link to="/about">About</Link>
-        <Link to="/examples">Examples</Link>
+        <IndexLink to="/" activeClassName="active" activeStyle={{ color: 'red' }}>Get Weather</IndexLink>
+        <Link to="/about" activeClassName="active" activeStyle={{ color: 'red' }}>About</Link>
+        <Link to="/examples" activeClassName="active" activeStyle={{ color: 'red' }}>Examples</Link>
       </div>
     );
   }
