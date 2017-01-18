d69354a27e2c2628b4b61c3015a819f03ba9302a
Dušan Miletić <d@miletich.cc>
2017-01-18 17:33:42 +0100

    Add loading indicator

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,31 +7,43 @@ class Weather extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: 'Belgrade',
-      temp: '30'
-    }
+      isLoading: false
+    };
     this.handleSearch = this.handleSearch.bind(this);
   }
 
   handleSearch(location){
     let that = this;
 
+    this.setState({isLoading: true});
+
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
-        temp: temp
+        temp: temp,
+        isLoading: false
       });
     }, function (errorMessage) {
+        this.setState({isLoading: false});
         alert(errorMessage);
     });
   }
 
   render() {
+    let {isLoading, temp, location } = this.state;
+
+    function renderMessage() {
+      if (isLoading) {
+        return <h3>Fetching weather...</h3>;
+      } else if (temp && location) {
+        return <WeatherMessage location={location} temp={temp}/>
+      }
+    }
     return (
       <div>
         <h3>Weather</h3>
         <WeatherForm onSearch={this.handleSearch}/>
-        <WeatherMessage location={this.state.location} temp={this.state.temp}/>
+        {renderMessage()}
       </div>
     );
   }
