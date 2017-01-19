4829e64064633cecd258ad281c5aaa8446f5df64
Dušan Miletić <d@miletich.cc>
2017-01-19 18:11:09 +0100

    Refactor navigation to use react-bootstrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import { Link, IndexLink } from 'react-router';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
+import { Navbar, NavItem, Nav, FormGroup, FormControl, Button } from 'react-bootstrap';
 
-const Navigation = (props) => {
-  return (
-    <div>
-      <h1>Navigation</h1>
-      <IndexLink to="/" activeClassName="active" activeStyle={{ color: 'red' }}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active" activeStyle={{ color: 'red' }}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{ color: 'red' }}>Examples</Link>
-    </div>
-  );
+class Navigation extends Component {
+  constructor(props) {
+    super();
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    alert('hello');
+  }
+
+  render() {
+    return (
+      <Navbar staticTop>
+        <Navbar.Header>
+           <Navbar.Brand>
+             React Weather App
+           </Navbar.Brand>
+           <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <IndexLinkContainer to="/">
+              <NavItem eventKey={1}>Get Weather</NavItem>
+            </IndexLinkContainer>
+            <LinkContainer to="/about">
+              <NavItem eventKey={2}>About</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/examples">
+              <NavItem eventKey={3}>Examples</NavItem>
+            </LinkContainer>
+          </Nav>
+          <Navbar.Form pullRight>
+            <form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <FormControl type="text" placeholder="Search..."/>
+              </FormGroup>
+              {'  '}
+              <Button bsStyle="primary" type="submit">Get Weather</Button>
+            </form>
+          </Navbar.Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 export default Navigation;
